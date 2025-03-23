e047cf69b06ec873450aaec4e2dceb69f79453bd
Alex Antolin <aantolin@nettrim.es>
2025-03-23 20:23:33 +0100

    Create new structure of directory

---
+++ frontend/.angular/cache/19.2.4/trainingzone/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,17 @@
+import "./chunk-M3HR6BUY.js";
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-VJ5AFMEN.js";
+import "./chunk-RLI5KEP3.js";
+import "./chunk-N2APCJKI.js";
+import "./chunk-P6U2JBMQ.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ frontend/.angular/cache/19.2.4/trainingzone/vite/deps/@angular_material_divider.js
@@ -0,0 +1,17 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-UP5XO7HP.js";
+import "./chunk-RXVQCC6F.js";
+import "./chunk-3LQK72XT.js";
+import "./chunk-M3HR6BUY.js";
+import "./chunk-2CWHE4TP.js";
+import "./chunk-VJ5AFMEN.js";
+import "./chunk-RLI5KEP3.js";
+import "./chunk-N2APCJKI.js";
+import "./chunk-P6U2JBMQ.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
