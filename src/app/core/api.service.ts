f7192c7b09227f5add5bbb7abd7e47507340664f
Alex Antolin <aantolin@nettrim.es>
2025-03-25 23:48:10 +0100

    Test

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = '/api/test';
+  private apiUrl = '/api/db';
 
   constructor(private http: HttpClient) {}
 
