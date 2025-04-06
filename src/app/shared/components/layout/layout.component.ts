4a7216771018e397e6bb53bb126cd874f321d80f
Alex <alejandro.antolin.92@gmail.com>
2025-04-06 11:33:42 +0200

    Resize and optimize images

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenavContent } from '@angular/material/sidenav';
 import { NavigationEnd, Router } from '@angular/router';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { TranslateService } from '@ngx-translate/core';
 import { filter } from 'rxjs';
 
@@ -20,7 +21,7 @@ export class LayoutComponent {
   @ViewChild(MatSidenavContent) sidenavContent!: MatSidenavContent;
 
   activePath: string = '';
-
+  faBars = faBars;
   menuItems: MenuItem[] = [
     {
       name: 'Inicio',
